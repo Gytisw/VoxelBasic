2ba8835b1c370eb3f5c7aee3b19927f715afc875
Gyitsw <gytis.weirdbox@gmail.com>
2025-09-02 00:25:24 +0200

    Layer handlers for first biome.

--- Assets/_Scripts/BiomeGenerator.cs
+++ Assets/_Scripts/BiomeGenerator.cs
@@ -7,6 +7,7 @@ public class BiomeGenerator : MonoBehaviour
 {
     public int waterThreshold = 50;
     public NoiseSettings biomeNoiseSettings;
+    public BlockLayerHandler startLayerHandler;
     
     public ChunkData ProcessChunkColumn(ChunkData data, int x, int z, Vector2Int mapSeedOffset)
     {
@@ -14,29 +15,30 @@ public class BiomeGenerator : MonoBehaviour
         int groundPosition = GetSurfaceHeightNoise(data.worldPosition.x + x, data.worldPosition.z + z, data.chunkHeight);
         for (int y = 0; y < data.chunkHeight; y++)
         {
-            BlockType voxelType = BlockType.Dirt;
-            if (y > groundPosition)
-            {
-                if (y < waterThreshold)
-                {
-                    voxelType = BlockType.Water;
-                }
-                else
-                {
-                    voxelType = BlockType.Air;
-                }
-
-            }
-            else if (y == groundPosition && y < waterThreshold)
-            {
-                voxelType = BlockType.Sand;
-            }
-            else if (y == groundPosition)
-            {
-                voxelType = BlockType.GrassDirt;
-            }
-
-            Chunk.SetBlock(data, new Vector3Int(x, y, z), voxelType);
+            startLayerHandler.Handle(data, x, y, z, groundPosition, mapSeedOffset);
+            // BlockType voxelType = BlockType.Dirt;
+            // if (y > groundPosition)
+            // {
+            //     if (y < waterThreshold)
+            //     {
+            //         voxelType = BlockType.Water;
+            //     }
+            //     else
+            //     {
+            //         voxelType = BlockType.Air;
+            //     }
+            //
+            // }
+            // else if (y == groundPosition && y < waterThreshold)
+            // {
+            //     voxelType = BlockType.Sand;
+            // }
+            // else if (y == groundPosition)
+            // {
+            //     voxelType = BlockType.GrassDirt;
+            // }
+            //
+            // Chunk.SetBlock(data, new Vector3Int(x, y, z), voxelType);
         }
         return data;
     }
