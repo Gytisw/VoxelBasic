cd6a3c99112bff4752e1bccfb3a950c741dc1749
Gyitsw <gytis.weirdbox@gmail.com>
2025-09-02 00:59:37 +0200

    Additional Layers

--- Assets/_Scripts/BiomeGenerator.cs
+++ Assets/_Scripts/BiomeGenerator.cs
@@ -8,6 +8,7 @@ public class BiomeGenerator : MonoBehaviour
     public int waterThreshold = 50;
     public NoiseSettings biomeNoiseSettings;
     public BlockLayerHandler startLayerHandler;
+    public List<BlockLayerHandler> additionalLayerHandlers;
     
     public ChunkData ProcessChunkColumn(ChunkData data, int x, int z, Vector2Int mapSeedOffset)
     {
@@ -16,29 +17,11 @@ public class BiomeGenerator : MonoBehaviour
         for (int y = 0; y < data.chunkHeight; y++)
         {
             startLayerHandler.Handle(data, x, y, z, groundPosition, mapSeedOffset);
-            // BlockType voxelType = BlockType.Dirt;
-            // if (y > groundPosition)
-            // {
-            //     if (y < waterThreshold)
-            //     {
-            //         voxelType = BlockType.Water;
-            //     }
-            //     else
-            //     {
-            //         voxelType = BlockType.Air;
-            //     }
-            //
-            // }
-            // else if (y == groundPosition && y < waterThreshold)
-            // {
-            //     voxelType = BlockType.Sand;
-            // }
-            // else if (y == groundPosition)
-            // {
-            //     voxelType = BlockType.GrassDirt;
-            // }
-            //
-            // Chunk.SetBlock(data, new Vector3Int(x, y, z), voxelType);
+        }
+        
+        foreach (var layer in additionalLayerHandlers)
+        {
+            layer.Handle(data, x, data.worldPosition.y, z, groundPosition, mapSeedOffset);
         }
         return data;
     }
