2c56ce2ebe4f799aef0ebd2e68a6d72583a95d1c
Gyitsw <gytis.weirdbox@gmail.com>
2025-09-02 04:21:14 +0200

    Camera + Basic Controls

---
+++ Assets/_Scripts/Player/Character.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+    [SerializeField]
+    private PlayerInput playerInput;
+    [SerializeField]
+    private PlayerMovement playerMovement;
+    
+    public float interactionRayLength = 5f;
+
+    public LayerMask groundMask;
+    public bool fly = false;
+    
+    public Animator animator;
+    
+    bool isWaiting = false;
+    
+    private void Awake()
+    {
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+        playerInput = GetComponent<PlayerInput>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+    
+    private void Start()
+    {
+        playerInput.OnMouseClick += HandleMouseClick;
+        playerInput.OnFly += HandleFlyClick;
+    }
+
+    private void HandleFlyClick()
+    {
+        fly = !fly;
+    }
+
+    void Update()
+    {
+        if (fly)
+        {
+            animator.SetFloat("Speed", 0f);
+            animator.SetBool("isGrounded", false);
+            animator.ResetTrigger("Jump");
+            playerMovement.Fly(playerInput.MovementInput, playerInput.IsJumping, playerInput.RunningPressed);
+        }
+        else
+        {
+            animator.SetBool("isGrounded", playerMovement.IsGrounded);
+            if (playerMovement.IsGrounded && playerInput.IsJumping && !isWaiting)
+            {
+                animator.SetTrigger("Jump");
+                isWaiting = true;
+                StopAllCoroutines();
+                StartCoroutine(ResetWaiting());
+            }
+            animator.SetFloat("speed", playerInput.MovementInput.magnitude);
+            playerMovement.HandleGravity(playerInput.IsJumping);
+            playerMovement.Walk(playerInput.MovementInput, playerInput.RunningPressed);
+        }
+    }
+
+    IEnumerator ResetWaiting()
+    {
+        yield return new WaitForSeconds(0.1f);
+        animator.ResetTrigger("jump");
+        isWaiting = false;
+    }
+
+    private void HandleMouseClick()
+    {
+        
+    }
+}
